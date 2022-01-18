c9a68bae89492fd550b66bc1099bf2e8c61108e9
vayne <flashvayne@gmail.com>
2022-01-18 22:19:51 +0800

    Maven项目初始化

---
+++ src/main/java/com/github/flashvayne/EasyRbacApplication.java
@@ -0,0 +1,18 @@
+package com.github.flashvayne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author flashvayne
+ */
+@SpringBootApplication
+public class EasyRbacApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(EasyRbacApplication.class,args);
+    }
+
+}
