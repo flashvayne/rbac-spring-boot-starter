1b4072f990404e33e4386629b079b797bf7c202a
flashvayne <flashvayne@gmail.com>
2022-01-21 19:44:48 +0800

    注解注释

--- src/main/java/com/github/flashvayne/aspect/RbacAuthorization.java
+++ src/main/java/com/github/flashvayne/aspect/RbacAuthorization.java
@@ -1,4 +1,4 @@
-package com.github.flashvayne.config;
+package com.github.flashvayne.aspect;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 /**
  * Rbac鉴权注解
+ * 在需要身份验证及权限校验的方法上使用此注解
  *
  * @author flashvayne
  */
