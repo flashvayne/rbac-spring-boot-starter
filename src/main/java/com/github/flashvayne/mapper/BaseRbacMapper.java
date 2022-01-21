0ecacb3ce9d630b05d62ebd5d88f340ec473fd7a
flashvayne <flashvayne@gmail.com>
2022-01-21 19:44:17 +0800

    Dao MySQL的默认实现

--- src/main/java/com/github/flashvayne/mapper/BaseRbacMapper.java
+++ src/main/java/com/github/flashvayne/mapper/BaseRbacMapper.java
@@ -7,8 +7,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+/**
+ * Rbac Dao
+ * Dao基础接口，本项目提供了MySQL的实现RbacMapper-MySQL.xml
+ * 其他数据源请继承此接口进行实现
+ *
+ * @author flashvayne
+ */
 @Mapper
-public interface UserMapper {
+public interface BaseRbacMapper {
     String selectPassword(String userId);
     AuthUserDTO selectUser(String userId);
     List<AuthRoleDTO> selectRole(String userId);
