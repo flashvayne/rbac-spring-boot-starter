01d1759ec74ea466015193809c6380b3923244b9
flashvayne <flashvayne@gmail.com>
2022-01-21 19:45:44 +0800

    Rbac自动装配

---
+++ src/main/java/com/github/flashvayne/property/RbacProperties.java
@@ -0,0 +1,36 @@
+package com.github.flashvayne.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Rbac配置属性类
+ *
+ * @author flashvayne
+ */
+@Component
+@ConfigurationProperties(prefix = "rbac")
+public class RbacProperties {
+
+    //token过期时间
+    private Long tokenExpireTime = 7200L;
+
+    //token属性名
+    private String tokenName = "authorization";
+
+    public Long getTokenExpireTime() {
+        return tokenExpireTime;
+    }
+
+    public void setTokenExpireTime(Long tokenExpireTime) {
+        this.tokenExpireTime = tokenExpireTime;
+    }
+
+    public String getTokenName() {
+        return tokenName;
+    }
+
+    public void setTokenName(String tokenName) {
+        this.tokenName = tokenName;
+    }
+}
